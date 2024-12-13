383e3d3ac8fab2396f08096cf7180fe0c458da16
itismita77 <itismitaparida77@gmail.com>
2024-12-13 07:48:01 +0000

    variables done

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,19 @@
+// const_accountId = 144553
+const accountId = 12345; 
+let accountEmail = "itismita@gmail.com"
+var accountPassword ="213435"
+accountCity = "Jajpur"
+
+// accountId = 2
+/* 
+prefer not to use var
+*/
+
+let accountState =  " odisha"
+ accountEmail = "hc@hc.com"
+accountPassword = " ghghj"
+accountCity = "bhubaneswar"
+console.log(accountId); 
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// alert (3+5)      
+// we are using node not browser
+
+console.log ("Iti")
+console.log(3+6)
